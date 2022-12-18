f7957da952d43320be3625752f09d37b426915c2
Nidhal Haddad <haddadnidhal20@gmail.com>
2022-12-18 18:13:30 +0100

    first version

---
+++ Core/Inc/app_bluenrg.h
@@ -0,0 +1,13 @@
+/*
+ * app_bluenrg.h
+ *
+ *  Created on: Dec 18, 2022
+ *      Author: hadda
+ */
+
+#ifndef INC_APP_BLUENRG_H_
+#define INC_APP_BLUENRG_H_
+
+
+
+#endif /* INC_APP_BLUENRG_H_ */
---
+++ Core/Src/app_bluenrg.c
@@ -0,0 +1,36 @@
+/*
+ * app_bluenrg.c
+ *
+ *  Created on: Dec 18, 2022
+ *      Author: hadda
+ */
+#include "bluenrg_conf.h"
+#include "hci.h"
+#include "bluenrg1_types.h"
+#include "bluenrg1_gap.h"
+#include "bluenrg1_aci.h"
+#include "bluenrg1_hci_le.h"
+#include "app_bluenrg.h"
+
+
+
+
+void bluenrg_init(void)
+{
+	const char *name="Trix" ;
+	/*Initialize HCI*/
+	/*Reset HCI*/
+	/*Configure device address*/
+	/*Initialize Gatt server*/
+	/*Initialize GAP service*/
+	/*Update characteristics*/
+	/*Add custom service*/
+}
+
+
+
+
+
+
+
+
